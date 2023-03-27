994b9cbbc33c62bf3884700e5bca61c62e7850e8
Janika Savela <janika_s@hotmail.com>
2023-03-27 12:27:13 +0300

    first commit

---
+++ backend/src/test/java/com/climateapp/climateapp/ClimateappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climateapp.climateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimateappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
